b9142794b6bc391880afa090c1028103822cfadb
Andrei_Tsylko <Andrei_Tsylko@epam.com>
2021-12-12 14:31:46 +0100

    task-8: example of usage

--- src/dijkstra-algorithm.js
+++ src/dijkstra-algorithm.js
@@ -65,3 +65,17 @@ let findShortestPath = (graph, startNode, endNode) => {
         path: getShortestPastOutput(parents, endNode),
     };
 };
+
+
+// EXAMPLE of usage
+
+const graph = {
+    start: { A: 5, B: 2 },
+    A: { start: 1, C: 4, D: 2 },
+    B: { A: 8, D: 7 },
+    C: { D: 6, finish: 3 },
+    D: { finish: 1 },
+    finish: {},
+};
+
+console.log(findShortestPath(graph, "start", "end"));
